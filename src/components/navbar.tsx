88f3a34f711e71479293faff4627f47852f8f636
Youssef Idrissi <ykhoulani@gmail.com>
2026-01-12 14:22:29 +0100

    Fix Navbar authentication display and update TypeScript configuration

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -19,7 +19,7 @@ export function Navbar({isAuthenticated}: {isAuthenticated: boolean}) {
       {/* <button className="rounded-full bg-purple-600 px-4 py-2 text-sm font-semibold hover:bg-purple-500">
         Start Now
       </button> */}
-      {isAuthenticated ? null : (
+      {isAuthenticated ? <p>hello</p> : (
         <div className="flex gap-4">
         <Link href="/sign-in" className="underline">
           Sign In
